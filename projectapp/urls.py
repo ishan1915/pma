0351f311910055a17a25128024386123612a8e9a
Ishan <211b145@juetguna.in>
2025-08-25 00:13:10 +0530

    Add pk parameter to workspaceupdate URL pattern
    Updated the 'workspaceupdate' route to include a primary key parameter for identifying the workspace to update. This change enables the WorkspaceUpdateAPI to receive the workspace ID via the URL.

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('signup/',SignupAPI.as_view(),name='signup'),
     path('login/',LoginView.as_view(),name='login'),
     path('workspacecreate/',WorkspaceCreateAPI.as_view(),name='workspacecreate'),
-    path('workspaceupdate/',WorkspaceUpdateAPI.as_view(),name='workspaceupdate'),
+    path('workspaceupdate/<int:pk>/',WorkspaceUpdateAPI.as_view(),name='workspaceupdate'),
     path('workspace_members/<int:workspace_id>/',WorkspaceMemberList.as_view(),name='workspace_member'),
 
     path('projectcreate/',ProjectCreateAPI.as_view(),name='projectcreate'),
